8185df8e4177c730c25c35abc9d2bceda15d75ae
Christopher Kirkwood <christopherkirkwood@Christophers-MacBook-Air.local>
2021-06-28 14:06:00 -0400

    init

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,16 +1,16 @@
 class DogsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
+    if current_user
       dog = Dog.new(
         name: params[:name],
         age: params[:age],
         breed: params[:breed]
       )
       dog.save
-      rander json: {"it worked"}
+      render json: dog.as_json
     else
-      render json: {}, status: :unauthorized
+      render json: {message: "users must be logged in"}
     end
   end
 end
